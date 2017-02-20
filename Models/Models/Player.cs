d4b9fa07b6d53eb7ad7495880922fe88429a657d
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2017-02-21 01:17:04 +0200

    commenting the models validations for testing purposes

--- Models/Models/Player.cs
+++ Models/Models/Player.cs
@@ -11,31 +11,36 @@ namespace Tournaments.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Required]
-        [MinLength(2)]
-        [StringLength(50)]
+        //[Required]
+        //[MinLength(2)]
+        //[StringLength(50)]
         public string FirstName { get; set; }
 
-        [Required]
-        [MinLength(2)]
-        [StringLength(50)]
+        //[Required]
+        //[MinLength(2)]
+        //[StringLength(50)]
         public string LastName { get; set; }
 
-        [Required]
-        [MinLength(2)]
-        [StringLength(50)]
+        //[Required]
+        //[MinLength(2)]
+        //[StringLength(50)]
         public string NickName { get; set; }
 
         public string Picture { get; set; }
 
-        [Required]
-        [MinLength(8)]
-        [StringLength(50)]
+        //[Required]
+        //[MinLength(8)]
+        //[StringLength(50)]
         public string Email { get; set; }
+
         public double? Rating { get; set; }
+
         public bool? IsCoach { get; set; }
+
         public string CV { get; set; }
+
         public int? TeamId { get; set; }
+
         public virtual Team Team { get; set; }
     }
 }
